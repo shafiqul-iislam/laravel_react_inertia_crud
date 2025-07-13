6b502f8a45fd3b68094868b13915b3af318af574
Shafiqul Islam <shafiqulislam78652@gmail.com>
2025-07-13 22:47:36 +0600

    on going post moduel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use PHPUnit\Event\Test\PostConditionErrored;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('posts', [PostController::class, 'index'])->name('posts');
+    
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
